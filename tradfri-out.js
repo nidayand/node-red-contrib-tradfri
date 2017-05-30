293af8664ade8fc4cb0c2fdf1daa8106d1a8288d
nidayand <pggithub@gothager.se>
2017-05-30 21:02:41 +0200

    First test with class

--- tradfri-out.js
+++ tradfri-out.js
@@ -1,11 +1,25 @@
-module.exports = function(RED) {
-    function LowerCaseNode(config) {
-        RED.nodes.createNode(this,config);
-        var node = this;
-        node.on('input', function(msg) {
-            msg.payload = msg.payload.toLowerCase();
-            node.send(msg);
-        });
-    }
-    RED.nodes.registerType("lower-case",LowerCaseNode);
+module.exports = function (RED) {
+  'use strict';
+  var rsvp = require('rsvp');
+
+  if (!RED.settings.functionGlobalContext.tradfri) {
+    RED.settings.functionGlobalContext.tradfri = require('node-tradfri-argon').create({
+      securityId: "2wsY1QSL65k3iD8i",
+      hubIpAddress: "192.168.2.213",
+      coapClientPath: "/home/user/libcoap/examples/coap-client"
+    });
+  }
+  var tradfri = RED.settings.functionGlobalContext.tradfri;
+
+  function TradfriNode(config) {
+    RED.nodes.createNode(this, config);
+    var node = this;
+    node.on('input', function (msg) {
+      tradfri.getAll().then(res => {
+        msg.payload = res;
+        node.send(msg);
+      });
+    });
+  }
+  RED.nodes.registerType("tradfri-out", TradfriNode);
 }
