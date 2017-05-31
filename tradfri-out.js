4d1b7bba4d7b5ae97d50c518f7a8e7721aa68b0e
nidayand <pggithub@gothager.se>
2017-05-31 09:53:57 +0200

    Added base config and framework

--- tradfri-out.js
+++ tradfri-out.js
@@ -2,18 +2,29 @@ module.exports = function (RED) {
   'use strict';
   var rsvp = require('rsvp');
 
-  if (!RED.settings.functionGlobalContext.tradfri) {
-    RED.settings.functionGlobalContext.tradfri = require('node-tradfri-argon').create({
-      securityId: "2wsY1QSL65k3iD8i",
-      hubIpAddress: "192.168.2.213",
-      coapClientPath: "/home/user/libcoap/examples/coap-client"
-    });
-  }
-  var tradfri = RED.settings.functionGlobalContext.tradfri;
-
+  /**
+   * Server side definition of tradfri node. Multiple options
+   * - either is the device or group configured in the node
+   * - incoming message defines the node to be managed. I.e. id is sent in message
+   *
+   * @author nidayand
+   * @param {object} config HTML configuration data
+   */
   function TradfriNode(config) {
     RED.nodes.createNode(this, config);
     var node = this;
+
+    // Configure the tradfri class as a global
+    if (!RED.settings.functionGlobalContext.tradfri) {
+      RED.settings.functionGlobalContext.tradfri = require('node-tradfri-argon').create({
+        securityId: "2wsY1QSL65k3iD8i",
+        hubIpAddress: "192.168.2.213",
+        coapClientPath: "/home/user/libcoap/examples/coap-client"
+      });
+    }
+    var tradfri = RED.settings.functionGlobalContext.tradfri;
+
+
     node.on('input', function (msg) {
       tradfri.getAll().then(res => {
         msg.payload = res;
@@ -22,4 +33,12 @@ module.exports = function (RED) {
     });
   }
   RED.nodes.registerType("tradfri-out", TradfriNode);
+
+  function TradfriConfigNode(config) {
+    RED.nodes.createNode(this, config);
+    var node = this;
+
+  }
+  RED.nodes.registerType("tradfri-config", TradfriConfigNode);
+
 }
